59ef4b20b6d2f705f4c05ee9b6afc2af4da0c2a7
Hoàng Trung Công <37680352+trungcong1999@users.noreply.github.com>
2021-04-26 01:22:09 +0700

    update

---
+++ wordpress/wp-content/themes/hosco/header.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="en"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <title>Công Ty Cổ Phần HOSCO</title>
+    <link rel="canonical" href="https://hosco.vn/" />
+    <link rel="icon" href="https://www.hosco.com.vn/wp-content/uploads/2019/05/icon-hosco.png">
+    <link href="<?php bloginfo("template_directory"); ?>/themes/default/assets/css/bootstrap4.min.css" type="text/css" rel="stylesheet"/>
+    <link rel="stylesheet" href="<?php bloginfo("template_directory"); ?>/backend/css/plugins.css">
+    <link rel="stylesheet" href="<?php bloginfo("template_directory"); ?>assets/vendor/toastr/toastr.min.css">
+    <link rel="stylesheet" href="<?php bloginfo("template_directory"); ?>/assets/vendor/sweetalert2/sweetalert2.min.css">
+    <link rel="stylesheet" href="<?php bloginfo("template_directory"); ?>/assets/vendor/flag/css/flag-icon.min.css">
+    <link rel="stylesheet" href="<?php bloginfo("template_directory"); ?>/themes/default/assets/css/swiper-bundle.min.css">
+    <link href="<?php bloginfo("template_directory"); ?>/themes/default/assets/css/animate.css" type="text/css" rel="stylesheet"/>
+    <link rel="stylesheet" href="<?php bloginfo("template_directory"); ?>/assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?php bloginfo("template_directory"); ?>/assets/css/owl.theme.default.min.css">
+
+    <style>
+        .slick-slider{position:relative;display:block;-moz-box-sizing:border-box;box-sizing:border-box;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;-webkit-touch-callout:none;-khtml-user-select:none;-ms-touch-action:pan-y;touch-action:pan-y;-webkit-tap-highlight-color:transparent}.slick-list{position:relative;display:block;overflow:hidden;margin:0;padding:0}.slick-list:focus{outline:0}.slick-list.dragging{cursor:pointer;cursor:hand}.slick-slider .slick-list,.slick-slider .slick-track{-webkit-transform:translate3d(0,0,0);-moz-transform:translate3d(0,0,0);-ms-transform:translate3d(0,0,0);-o-transform:translate3d(0,0,0);transform:translate3d(0,0,0)}.slick-track{position:relative;top:0;left:0;display:block}.slick-track:after,.slick-track:before{display:table;content:''}.slick-track:after{clear:both}.slick-loading .slick-track{visibility:hidden}.slick-slide{display:none;float:left;height:100%;min-height:1px}[dir=rtl] .slick-slide{float:right}.slick-slide img{display:block}.slick-slide.slick-loading img{display:none}.slick-slide.dragging img{pointer-events:none}.slick-initialized .slick-slide{display:block}.slick-loading .slick-slide{visibility:hidden}.slick-vertical .slick-slide{display:block;height:auto;border:1px solid transparent}.slick-arrow.slick-hidden{display:none}.slick-slide,.slick-slide a,.slick-slide img,.slick-slide:active,.slick-slide:focus{outline:0}
+        /*Khong cho load cac slider slick thi load trang */
+        .case_study:not(.slick-initialized) , .talk-cnv-cas:not(.slick-initialized)
+        , .solution_app_mobile:not(.slick-initialized) , .four_post_bottom_banner_mobile:not(.slick-initialized),
+        , .nav-cas-mobile:not(.slick-initialized) , .customers_partner_mobile:not(.slick-initialized) {
+          display: none;
+      }
+      @media  screen and (min-width: 1024px){
+        .four_post_bottom_banner_desktop{
+            margin-top: -70px
+        }
+        .four_post_bottom_banner{
+            margin-top: 75px;
+        }
+    }
+</style>
+<link href="<?php bloginfo("template_directory"); ?>/themes/default/assets/css/elegant-icons.min.css" type="text/css" rel="stylesheet"/>
+<style>
+    .fancybox-wrap,.fancybox-skin,.fancybox-outer,.fancybox-inner,.fancybox-image,.fancybox-wrap iframe,.fancybox-wrap object,.fancybox-nav,.fancybox-nav span,.fancybox-tmp{padding:0;margin:0;border:0;outline:none;vertical-align:top}.fancybox-wrap{position:absolute;top:0;left:0;z-index:8020}.fancybox-skin{position:relative;background:#f9f9f9;color:#444;text-shadow:none;-webkit-border-radius:4px;-moz-border-radius:4px;border-radius:4px}.fancybox-opened{z-index:8030}.fancybox-opened .fancybox-skin{-webkit-box-shadow:0 10px 25px rgba(0,0,0,0.5);-moz-box-shadow:0 10px 25px rgba(0,0,0,0.5);box-shadow:0 10px 25px rgba(0,0,0,0.5)}.fancybox-outer,.fancybox-inner{position:relative}.fancybox-inner{overflow:hidden}.fancybox-type-iframe .fancybox-inner{-webkit-overflow-scrolling:touch}.fancybox-error{color:#444;font:14px/20px "Helvetica Neue",Helvetica,Arial,sans-serif;margin:0;padding:15px;white-space:nowrap}.fancybox-image,.fancybox-iframe{display:block;width:100%;height:100%}.fancybox-image{max-width:100%;max-height:100%}#fancybox-loading,.fancybox-close,.fancybox-prev span,.fancybox-next span{background-image:url(fancybox_sprite.png)}#fancybox-loading{position:fixed;top:50%;left:50%;margin-top:-22px;margin-left:-22px;background-position:0 -108px;opacity:.8;cursor:pointer;z-index:8060}#fancybox-loading div{width:44px;height:44px;background:url(fancybox_loading.gif) center center no-repeat}.fancybox-close{position:absolute;top:-18px;right:-18px;width:36px;height:36px;cursor:pointer;z-index:8040}.fancybox-nav{position:absolute;top:0;width:40%;height:100%;cursor:pointer;text-decoration:none;background:transparent url(blank.gif);-webkit-tap-highlight-color:rgba(0,0,0,0);z-index:8040}.fancybox-prev{left:0}.fancybox-next{right:0}.fancybox-nav span{position:absolute;top:50%;width:36px;height:34px;margin-top:-18px;cursor:pointer;z-index:8040;visibility:hidden}.fancybox-prev span{left:10px;background-position:0 -36px}.fancybox-next span{right:10px;background-position:0 -72px}.fancybox-nav:hover span{visibility:visible}.fancybox-tmp{position:absolute;top:-99999px;left:-99999px;visibility:hidden;max-width:99999px;max-height:99999px;overflow:visible!important}.fancybox-lock{overflow:hidden!important;width:auto}.fancybox-lock body{overflow:hidden!important}.fancybox-lock-test{overflow-y:hidden!important}.fancybox-overlay{position:absolute;top:0;left:0;overflow:hidden;display:none;z-index:8010;background:url(fancybox_overlay.png)}.fancybox-overlay-fixed{position:fixed;bottom:0;right:0}.fancybox-lock .fancybox-overlay{overflow:auto;overflow-y:scroll}.fancybox-title{visibility:hidden;font:normal 13px/20px "Helvetica Neue",Helvetica,Arial,sans-serif;position:relative;text-shadow:none;z-index:8050}.fancybox-opened .fancybox-title{visibility:visible}.fancybox-title-float-wrap{position:absolute;bottom:0;right:50%;margin-bottom:-35px;z-index:8050;text-align:center}.fancybox-title-float-wrap .child{display:inline-block;margin-right:-100%;padding:2px 20px;background:transparent;background:rgba(0,0,0,0.8);-webkit-border-radius:15px;-moz-border-radius:15px;border-radius:15px;text-shadow:0 1px 2px #222;color:#FFF;font-weight:700;line-height:24px;white-space:nowrap}.fancybox-title-outside-wrap{position:relative;margin-top:10px;color:#fff}.fancybox-title-inside-wrap{padding-top:10px}.fancybox-title-over-wrap{position:absolute;bottom:0;left:0;color:#fff;padding:10px;background:#000;background:rgba(0,0,0,.8)}@media  only screen and (-webkit-min-device-pixel-ratio:1.5),only screen and (min--moz-device-pixel-ratio:1.5),only screen and (min-device-pixel-ratio:1.5){#fancybox-loading,.fancybox-close,.fancybox-prev span,.fancybox-next span{background-image:url(fancybox_sprite@2x.png);background-size:44px 152px}#fancybox-loading div{background-image:url(fancybox_loading@2x.gif);background-size:24px 24px}}
+</style>
+<style>
+    #fancybox-thumbs{position:fixed;left:0;width:100%;overflow:hidden;z-index:8050}#fancybox-thumbs.bottom{bottom:2px}#fancybox-thumbs.top{top:2px}#fancybox-thumbs ul{position:relative;list-style:none;margin:0;padding:0}#fancybox-thumbs ul li{float:left;padding:1px;opacity:.5}#fancybox-thumbs ul li.active{opacity:.75;padding:0;border:1px solid #fff}#fancybox-thumbs ul li:hover{opacity:1}#fancybox-thumbs ul li a{display:block;position:relative;overflow:hidden;border:1px solid #222;background:#111;outline:none}#fancybox-thumbs ul li img{display:block;position:relative;border:0;padding:0;max-width:none}
+</style>
+<link href="<?php bloginfo("template_directory"); ?>/themes/default/assets/css/style.css" type="text/css" rel="stylesheet"/>
+<link href="<?php bloginfo("template_directory"); ?>/themes/default/css/cnv.css" type="text/css" rel="stylesheet">
+<link href="<?php bloginfo("template_directory"); ?>/themes/default/css/cnv_new.css" type="text/css" rel="stylesheet">
+<style>
+    [class*=cnv-img-]{display:flex!important;justify-content:center}.cnv-img-square{position:relative;padding-top:100%;display:block;overflow:hidden}.cnv-img-rectangle{position:relative;padding-top:75%;display:block;overflow:hidden}.cnv-img-3x4{position:relative;padding-top:132.5%;display:block;overflow:hidden}.cnv-img-3x2{position:relative;padding-top:66.5%;display:block;overflow:hidden}.cnv-img-16x9{position:relative;padding-top:56.5%;display:block;overflow:hidden}.cnv-img-16x9 img,.cnv-img-3x2 img,.cnv-img-4x3 img,.cnv-img-rectangle img,.cnv-img-square img{transition:all .4s ease-in-out 0s;-moz-transition:all .4s ease-in-out 0s;-ms-transition:all .4s ease-in-out 0s;-webkit-transition:all .4s ease-in-out 0s;-o-transition:all .4s ease-in-out 0s;position:absolute;width:auto;height:auto;top:0;bottom:0;width:100%;height:auto;max-width:100%;max-height:100%;margin: 0 auto;}.cnv-img{padding:0;display:block;position:relative;overflow:hidden;text-align:center}.cnv-img:before{-webkit-transition:all .2s ease-in-out;transition:all .2s ease-in-out;content:'';position:absolute;top:0;left:0;bottom:0;right:0;z-index:10}.cnv-img:hover:before{background-color:rgba(51,150,243,.4)}.cnv-img img{-webkit-transition:all .4s ease-in-out 0s;transition:all .4s ease-in-out 0s}.cnv-img:hover img{transform:scale(1.1)}.cnv-smooth{-webkit-transition:all .4s ease-in-out 0s;transition:all .4s ease-in-out 0s}
+</style>
+<style>
+    @media  screen and (max-width: 991px){
+        header{
+            background: transparent !important;
+        }
+    }
+</style>
+<style>
+    .social_footer .fa{display:inline-block;font:normal normal normal 14px/1 FontAwesome;font-size:inherit;text-rendering:auto;-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale}.social_footer a{width:32px;height:32px;border-radius:0;display:inline-block;font-size:16px;border:1px solid #fff;margin:0 3px;color:#fff;text-align:center;padding-top:3px;margin:0 auto;margin-right:8px;overflow:hidden;position:relative}.social_footer a:hover{color:#fff}.social_footer a img{width:32px;height:32px}.fa-facebook-f:before,.fa-facebook:before{content:"\f09a"}.social_footer a.f{background:#104f94}.social_footer a.t{background:#1da1f2}.social_footer a.y{background:#c30000}.social_footer a.l{background:#1da1f2}ul.primary-menu>li .cnv-submenu li .icon.career{position:absolute;display:block;width:65px;height:65px;left:18px;top:10px}
+</style>
+
+<script src="<?php bloginfo("template_directory"); ?>/assets/js/jssor.slider-28.1.0.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+    window.jssor_1_slider_init = function() {
+
+        var jssor_1_options = {
+          $AutoPlay: 1,
+          $Idle: 0,
+          $SlideDuration: 5000,
+          $SlideEasing: $Jease$.$Linear,
+          $PauseOnHover: 4,
+          $SlideWidth: 140,
+          $Align: 0
+      };
+
+      var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+      var jssor_2_slider = new $JssorSlider$("jssor_2", jssor_1_options);
+      var jssor_3_slider = new $JssorSlider$("jssor_3", jssor_1_options);
+      var jssor_3_slider = new $JssorSlider$("jssor_4", jssor_1_options);
+
+  };
+</script>
+</head>
+<body class="">
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PLFDCD7"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <div class="wrapper">
+            <header class="mainHeader-hrv mainHeader-primary">
+                <div class="header-bottom">
+                    <nav class="navbar navbarmain-primary hidden-xs">
+                        <div class="container-primary container-fluid">
+                            <div class="wrapper-navbar-primary">
+                                <div class="navsub-primary-menu flex-align-primary">
+                                    <div class="navbar-logo">
+                                        <a class="logo-primary" href="index.html">
+                                            <img class="hide-scroll svg_logo_loyalty" src="https://www.hosco.com.vn/wp-content/uploads/2019/05/logo-hosco-Full.png" alt="">
+                                            <img class="show-scroll svg_iconlogo_loyalty" src="https://www.hosco.com.vn/wp-content/uploads/2019/05/logo-hosco-Full.png" alt="">
+                                        </a>
+                                    </div>
+                                    <span class="line-br"></span>
+                                    <ul class="list-unstyled nav navbar-nav primary-menu">
+                                        <li>
+                                            <a href="#">VỀ HOSCO</a>
+                                        </li>
+                                        <li>
+                                            <a href="tinh-nang.html">SẢN PHẨM</a>
+                                            <ul class="cnv-submenu row">
+                                                <div class="col-lg-6 col-xs-6 lineTransition">
+                                                    <li class="list-sub" style="padding: 0px 35px 0px 10px !important;"><h4 style="font-weight: bold;">Hệ sinh thái phần mềm Hosco</h4><hr class="inline"/></li>
+                                                    
+                                                    <li class="list-sub">
+                                                    <!-- <a class="smooth icon " href="#" title="">
+                                                        <img src="storage/icons/spa.png" alt="" title="">
+                                                        <img src="storage/icons/spa.png" alt="" title="">
+                                                    </a> -->
+                                                    <div class="content-sub-menu">
+                                                        <a class="title" href="<?php bloginfo("template_directory"); ?>/product.php" title="">Phần mềm bán hàng Master Pro</a>
+                                                    </div>
+                                                </li>
+                                                <li class="list-sub">
+
+                                                    <div class="content-sub-menu">
+                                                        <a class="title" href="#" title="">Phần mềm quản lý nhân sự Lotus Pro</a>
+                                                    </div>
+                                                </li>
+                                                <li class="list-sub">
+
+                                                    <div class="content-sub-menu">
+                                                        <a class="title" href="#" title="">Phần mềm quản lý phân phối Lotus DMS</a>
+                                                    </div>
+                                                </li>
+                                            </div>
+                                            <div class="col-lg-6 col-xs-6 lineTransition">
+                                                <li class="list-sub" style="padding: 0px 35px 0px 10px !important;"><h4 style="font-weight: bold;">Giải pháp quản lý doanh nghiệp</h4><hr class="inline"/></li>
+                                                <li class="list-sub">
+
+                                                    <div class="content-sub-menu">
+                                                        <a class="title" href="#" title="">Phần mềm bán hàng tích hợp Lotus ERP</a>
+                                                    </div>
+                                                </li>
+                                                <li class="list-sub">
+
+                                                    <div class="content-sub-menu">
+                                                        <a class="title" href="#" title="">Giải pháp quản lý nhân sự Lotus ERP</a>
+                                                    </div>
+                                                </li>
+                                                <li class="list-sub">
+
+                                                    <div class="content-sub-menu">
+                                                        <a class="title" href="#" title="">Phần mềm bán hàng tích hợp SAP B1</a>
+                                                    </div>
+                                                </li>
+                                            </div>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="#nganh-hang">KHÁCH HÀNG</a>
+                                    </li>
+                                    <li>
+                                        <a href="index.html">TUYỂN DỤNG</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">TIN TỨC</a>
+                                        
+                                    </li>
+                                    <li>
+                                        <a class="contact_animation" href="#lien-he">LIÊN HỆ</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <ul class="list-unstyled primary-button flex-align-primary">
+                                <li>
+                                    <a class="btn-menu-login btn-menu btn-hover-vertical btn-menu-loyalty" data-toggle="modal" data-target="#informationForm" href="#information-form">Trải nghiệm ngay</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                <button class="menu-btn nav-open" type="button"><i></i></button>
+            </header>
